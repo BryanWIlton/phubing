f86ffd4e16e07c5cb6b6691e853ce7e2a7325faa
Bryan Wilton <bryanwilton27@gmail.com>
2021-10-14 12:02:56 -0300

    first commit

---
+++ index/index.js
@@ -0,0 +1,11 @@
+
+function entrar(id) {
+    button = document.getElementById(id)
+    button.style.backgroundColor = 'rgba(255,0,0,0.5)'
+}
+
+
+function sair(id) {
+    button = document.getElementById(id)
+    button.style.backgroundColor = 'red'
+}
